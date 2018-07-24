2439005bf117c3f076b6ea33141067bfe3407332
Doug.Kuzenski <doug.kuzenski@parivedasolutions.com>
2018-07-23 17:03:25 -0700

    Cameron says "it seems to be working pretty well" Integration test passed!

--- find_person.py
+++ find_person.py
@@ -1,123 +1,197 @@
+#*****************************************************
+#                                                    *
+# Copyright 2018 Amazon.com, Inc. or its affiliates. *
+# All Rights Reserved.                               *
+#                                                    *
+#*****************************************************
+""" A sample lambda for object detection"""
+from threading import Thread, Event, Timer
 import os
-from threading import Timer
-import time
-import datetime
+import json
+import numpy as np
 import awscam
 import cv2
+import greengrasssdk
+import time
+from datetime import datetime, timedelta
 from botocore.session import Session
-from threading import Thread
 
 # Setup the S3 client
 session = Session()
 s3 = session.create_client('s3')
 s3_bucket = 'deeplens.seattle.doorman.test'
 
-# setup the camera and frame
-ret, frame = awscam.getLastFrame()
-ret,jpeg = cv2.imencode('.jpg', frame)
-Write_To_FIFO = True
-class FIFO_Thread(Thread):
-    def __init__(self):
-        ''' Constructor. '''
-        Thread.__init__(self)
+class LocalDisplay(Thread):
+    """ Class for facilitating the local display of inference results
+        (as images). The class is designed to run on its own thread. In
+        particular the class dumps the inference results into a FIFO
+        located in the tmp directory (which lambda has access to). The
+        results can be rendered using mplayer by typing:
+        mplayer -demuxer lavf -lavfdopts format=mjpeg:probesize=32 /tmp/results.mjpeg
+    """
+    def __init__(self, resolution):
+        """ resolution - Desired resolution of the project stream """
+        # Initialize the base class, so that the object can run on its own
+        # thread.
+        super(LocalDisplay, self).__init__()
+        # List of valid resolutions
+        RESOLUTION = {'1080p' : (1920, 1080), '720p' : (1280, 720), '480p' : (858, 480)}
+        if resolution not in RESOLUTION:
+            raise Exception("Invalid resolution")
+        self.resolution = RESOLUTION[resolution]
+        # Initialize the default image to be a white canvas. Clients
+        # will update the image when ready.
+        self.frame = cv2.imencode('.jpg', 255*np.ones([640, 480, 3]))[1]
+        self.stop_request = Event()
 
     def run(self):
-        # write to tmp file for local debugging purpose
-        fifo_path = "/tmp/results.mjpeg"
-        if not os.path.exists(fifo_path):
-            os.mkfifo(fifo_path)
-        f = open(fifo_path,'w')
+        """ Overridden method that continually dumps images to the desired
+            FIFO file.
+        """
+        # Path to the FIFO file. The lambda only has permissions to the tmp
+        # directory. Pointing to a FIFO file in another directory
+        # will cause the lambda to crash.
+        result_path = '/tmp/results.mjpeg'
+        # Create the FIFO file if it doesn't exist.
+        if not os.path.exists(result_path):
+            os.mkfifo(result_path)
+        # This call will block until a consumer is available
+        with open(result_path, 'w') as fifo_file:
+            while not self.stop_request.isSet():
+                try:
+                    # Write the data to the FIFO file. This call will block
+                    # meaning the code will come to a halt here until a consumer
+                    # is available.
+                    fifo_file.write(self.frame.tobytes())
+                except IOError:
+                    continue
 
-        # yay, succesful, let's start streaming to the file
-        while Write_To_FIFO:
-            try:
-                f.write(jpeg.tobytes())
-            except IOError as e:
-                continue
+    def set_frame_data(self, frame):
+        """ Method updates the image data. This currently encodes the
+            numpy array to jpg but can be modified to support other encodings.
+            frame - Numpy array containing the image data of the next frame
+                    in the project stream.
+        """
+        ret, jpeg = cv2.imencode('.jpg', cv2.resize(frame, self.resolution))
+        if not ret:
+            raise Exception('Failed to set frame data')
+        self.frame = jpeg
+
+    def join(self):
+        self.stop_request.set()
 
 def greengrass_infinite_infer_run():
+    """ Entry point of the lambda function"""
     try:
-        modelPath = "/opt/awscam/artifacts/mxnet_deploy_ssd_resnet50_300_FP16_FUSED.xml"
-        modelType = "ssd"
-        input_width = 300
+        # This object detection model is implemented as single shot detector (ssd), since
+        # the number of labels is small we create a dictionary that will help us convert
+        # the machine labels to human readable labels.
+        model_type = 'ssd'
+        output_map = {1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat', 5: 'bottle', 6: 'bus',
+                      7 : 'car', 8 : 'cat', 9 : 'chair', 10 : 'cow', 11 : 'dinning table',
+                      12 : 'dog', 13 : 'horse', 14 : 'motorbike', 15 : 'person',
+                      16 : 'pottedplant', 17 : 'sheep', 18 : 'sofa', 19 : 'train',
+                      20 : 'tvmonitor'}
+        # Create an IoT client for sending to messages to the cloud.
+        client = greengrasssdk.client('iot-data')
+        iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+        # Create a local display instance that will dump the image bytes to a FIFO
+        # file that the image can be rendered locally.
+        local_display = LocalDisplay('480p')
+        local_display.start()
+        # The sample projects come with optimized artifacts, hence only the artifact
+        # path is required.
+        model_path = '/opt/awscam/artifacts/mxnet_deploy_ssd_resnet50_300_FP16_FUSED.xml'
+        # Load the model onto the GPU.
+        client.publish(topic=iot_topic, payload='Loading object detection model')
+        model = awscam.Model(model_path, {'GPU': 1})
+        client.publish(topic=iot_topic, payload='Object detection model loaded')
+        # Set the threshold for detection
+        detection_threshold = 0.8
+        # The height and width of the training set images
         input_height = 300
-        max_threshold = 0.60  # raise/lower this value based on your conditions
-        outMap = { 1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat', 5: 'bottle', 6: 'bus', 7 : 'car', 8 : 'cat', 9 : 'chair', 10 : 'cow', 11 : 'dinning table', 12 : 'dog', 13 : 'horse', 14 : 'motorbike', 15 : 'person', 16 : 'pottedplant', 17 : 'sheep', 18 : 'sofa', 19 : 'train', 20 : 'tvmonitor' }
-        results_thread = FIFO_Thread()
-        results_thread.start()
+        input_width = 300
 
-        # Load model to GPU
-        mcfg = {"GPU": 1}
-        model = awscam.Model(modelPath, mcfg)
+        lastmatch = datetime.utcnow()
 
-        # try to get a frame from the camera
-        ret, frame = awscam.getLastFrame()
-        if ret == False:
-            raise Exception("Failed to get frame from the stream")
-
-        yscale = float(frame.shape[0]/input_height)
-        xscale = float(frame.shape[1]/input_width)
-
-        9- = True
-        while doInfer:
+        # Do inference until the lambda is killed.
+        while True:
             # Get a frame from the video stream
             ret, frame = awscam.getLastFrame()
+            if not ret:
+                raise Exception('Failed to get frame from the stream')
+            # Resize frame to the same size as the training set.
+            frame_resize = cv2.resize(frame, (input_height, input_width))
+            # Run the images through the inference engine and parse the results using
+            # the parser API, note it is possible to get the output of doInference
+            # and do the parsing manually, but since it is a ssd model,
+            # a simple API is provided.
+            parsed_inference_results = model.parseResult(model_type,
+                                                         model.doInference(frame_resize))
+            # Compute the scale in order to draw bounding boxes on the full resolution
+            # image.
+            yscale = float(frame.shape[0]/input_height)
+            xscale = float(frame.shape[1]/input_width)
+            # Dictionary to be filled with labels and probabilities for MQTT
+            cloud_output = {}
+            # Get the detected objects and probabilities
+            for obj in parsed_inference_results[model_type]:
+                if obj['prob'] > detection_threshold:
+                    # Add bounding boxes to full resolution frame
+                    xmin = int(xscale * obj['xmin']) \
+                           + int((obj['xmin'] - input_width/2) + input_width/2)
+                    ymin = int(yscale * obj['ymin'])
+                    xmax = int(xscale * obj['xmax']) \
+                           + int((obj['xmax'] - input_width/2) + input_width/2)
+                    ymax = int(yscale * obj['ymax'])
 
-            # Raise an exception if failing to get a frame
-            if ret == False:
-                raise Exception("Failed to get frame from the stream")
+                    # hold off for timespan between matches to let the system catch up
+                    if datetime.utcnow() > (lastmatch + timedelta(seconds=15)):
 
-            # Resize frame to fit model input requirement
-            frameResize = cv2.resize(frame, (input_width, input_height))
+                        try:
+                            # if a person was found, upload the target area to S3 for further inspection
+                            if output_map[obj['label']] == 'person':
 
-            # Run model inference on the resized frame
-            inferOutput = model.doInference(frameResize)
+                                # get the person image
+                                person = frame[ymin:ymax, xmin:xmax]
 
-            # Output inference result to the fifo file so it can be viewed with mplayer
-            parsed_results = model.parseResult(modelType, inferOutput)['ssd']
-            label = '{'
-            for obj in parsed_results:
-                if obj['prob'] > max_threshold:
-                    xmin = int( xscale * obj['xmin'] ) + int((obj['xmin'] - input_width/2) + input_width/2)
-                    ymin = int( yscale * obj['ymin'] )
-                    xmax = int( xscale * obj['xmax'] ) + int((obj['xmax'] - input_width/2) + input_width/2)
-                    ymax = int( yscale * obj['ymax'] )
+                                # create a nice s3 file key
+                                s3_key = datetime.utcnow().strftime('%Y-%m-%d_%H_%M_%S.%f') + '.jpg'
+                                encode_param=[int(cv2.IMWRITE_JPEG_QUALITY), 90]  # 90% should be more than enough
+                                _, jpg_data = cv2.imencode('.jpg', person, encode_param)
+                                filename = "incoming/%s" % s3_key  # the guess lambda function is listening here
+                                response = s3.put_object(ACL='public-read', Body=jpg_data.tostring(),Bucket=s3_bucket,Key=filename)
 
-                    # if a person was found, upload the target area to S3 for further inspection
-                    if outMap[obj['label']] == 'person':
+                                # reset the timer for the next match
+                                lastmatch = datetime.utcnow()
+                        except Exception as ex:
+                            client.publish(topic=iot_topic, payload='Error in person finder to S3 block: {}'.format(ex))
 
-                        # get the person image
-                        person = frame[ymin:ymax, xmin:xmax]
+                    # See https://docs.opencv.org/3.4.1/d6/d6e/group__imgproc__draw.html
+                    # for more information about the cv2.rectangle method.
+                    # Method signature: image, point1, point2, color, and tickness.
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 10)
+                    # Amount to offset the label/probability text above the bounding box.
+                    text_offset = 15
 
-                        # create a nice s3 file key
-                        s3_key = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H_%M_%S.%f') + '.jpg'
-                        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY), 90]  # 90% should be more than enough
-                        _, jpg_data = cv2.imencode('.jpg', person, encode_param)
-                        filename = "incoming/%s" % s3_key  # the guess lambda function is listening here
-                        response = s3.put_object(ACL='public-read', Body=jpg_data.tostring(),Bucket=s3_bucket,Key=filename)
+                    # See https://docs.opencv.org/3.4.1/d6/d6e/group__imgproc__draw.html
+                    # for more information about the cv2.putText method.
+                    # Method signature: image, text, origin, font face, font scale, color,
+                    # and tickness
+                    cv2.putText(frame, "{}: {:.2f}%".format(output_map[obj['label']],
+                                                               obj['prob'] * 100),
+                                (xmin, ymin-text_offset),
+                                cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 165, 20), 6)
+                    # Store label and probability to send to cloud
+                    cloud_output[output_map[obj['label']]] = obj['prob']
+            # Set the next frame in the local display stream.
+            local_display.set_frame_data(frame)
+            # Send results to the cloud
 
-                    # draw a rectangle around the designated area, and tell what label was found
-                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 4)
-                    label += '"{}": {:.2f},'.format(outMap[obj['label']], obj['prob'] )
-                    label_show = "{}:    {:.2f}%".format(outMap[obj['label']], obj['prob']*100 )
-                    cv2.putText(frame, label_show, (xmin, ymin-15),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 165, 20), 4)
-            label += '"null": 0.0'
-            label += '}'
+            if cloud_output != {}:
+                client.publish(topic=iot_topic, payload=json.dumps(cloud_output))
 
-            global jpeg
-            ret,jpeg = cv2.imencode('.jpg', frame)
+    except Exception as ex:
+        client.publish(topic=iot_topic, payload='Error in object detection lambda: {}'.format(ex))
 
-    except Exception as e:
-        print "Crap, something failed: %s" % str(e)
-
-    # Asynchronously schedule this function to be run again in 15 seconds
-    Timer(15, greengrass_infinite_infer_run).start()
-
-# Execute the function above
 greengrass_infinite_infer_run()
-
-
-# This is a dummy handler and will not be invoked
-# Instead the code above will be executed in an infinite loop for our example
-def function_handler(event, context):
-    return
