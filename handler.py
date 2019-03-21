39e566e188ca519cb38dabe53980e6c2607da5dc
Deborah Ang <debang35@gmail.com>
2019-03-21 11:21:45 -0400

    Add slack training channel id to lambda

--- handler.py
+++ handler.py
@@ -8,6 +8,7 @@ from urllib.parse import parse_qs
 bucket_name = os.environ['BUCKET_NAME']
 slack_token = os.environ['SLACK_API_TOKEN']
 slack_channel_id = os.environ['SLACK_CHANNEL_ID']
+slack_training_channel_id = os.environ['SLACK_TRAINING_CHANNEL_ID']
 rekognition_collection_id = os.environ['REKOGNITION_COLLECTION_ID']
 
 from doorman import guess
